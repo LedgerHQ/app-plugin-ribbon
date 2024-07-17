319910a7503408591b4d9f47d1603d59a878f7f1
Francois Beutin <francois.beutin@ledger.fr>
2024-07-17 18:20:22 +0200

    Remove unused variable

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -1,8 +1,6 @@
 #include "ribbon_plugin.h"
 
 void handle_provide_token(ethPluginProvideInfo_t *msg) {
-    context_t *context = (context_t *) msg->pluginContext;
-
     // Nothing to do here
 
     msg->result = ETH_PLUGIN_RESULT_OK;
